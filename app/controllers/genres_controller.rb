572e17dce51881154db015b1571f2f264ef8b352
spikeburton <ralphburton3@gmail.com>
2019-02-25 16:36:44 -0500

    add views for genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,33 @@
 class GenresController < ApplicationController
+  def show
+    @genre = genre
+  end
+
+  def new
+  end
+
+  def create
+    @genre = Genre.create(genre_params(:name))
+    redirect_to @genre
+  end
+
+  def edit
+    @genre = genre
+  end
+
+  def update
+    @genre = genre
+    @genre.update(genre_params(:name))
+    redirect_to @genre
+  end
+
+  private
+
+  def genre
+    Genre.find(params[:id])
+  end
+
+  def genre_params(*args)
+    params.require(:genre).permit(args)
+  end
 end
