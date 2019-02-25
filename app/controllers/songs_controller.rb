d0e94cab548799543130d9fb7a1bcc239c3c0fc0
spikeburton <ralphburton3@gmail.com>
2019-02-25 17:01:38 -0500

    add song views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,36 @@
 class SongsController < ApplicationController
+  def index
+  end
+
+  def show
+    @song = song
+  end
+
+  def new
+  end
+
+  def create
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
+    redirect_to @song
+  end
+
+  def edit
+    @song = song
+  end
+
+  def update
+    @song = song
+    @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to @song
+  end
+
+  private
+
+  def song
+    Song.find(params[:id])
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(args)
+  end
 end
